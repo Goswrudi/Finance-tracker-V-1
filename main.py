0d0603cf467f61ec4870b06ed6efa5f6c63a0db8
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-05 17:51:55 +0530

    Added the setting in function in main

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 
 # Information for the user
 
+import webbrowser
+
 def information():
     note = print('Welcome to the Fintraa !')
     name = input('Enter your name : ')
-    greets = input(f'''Hey {name} , Welcome to Fintraa !!  
+    greets = print(f'''Hey {name} , Welcome to Fintraa !!  
 The Job Eater of Accountant and Personal Assitant !! ''') 
     
 information()
+
+
+def setting():
+ 
+    G = print('''Google Web|Softwares key are ;
+S for Google Sheets 
+D for Google Docs
+P for Google PPT
+C for Google Calendor              
+GW for Google workspace              ''')
+
+# 1 = input('To See all Settings/keybord shortcut press * ')
+
+# if(1 == '*'):
