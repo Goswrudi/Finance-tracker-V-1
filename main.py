7cff8bd57ebfde8d67cfda6782eb2c685f89ce8d
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-05 17:01:58 +0530

    Added a information function

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# Information for the user
+
+def information():
+    note = print('Welcome to the Fintraa !')
+    name = input('Enter your name : ')
+    greets = input(f'''Hey {name} , Welcome to Fintraa !!  
+The Job Eater of Accountant and Personal Assitant !! ''') 
+    
+information()
