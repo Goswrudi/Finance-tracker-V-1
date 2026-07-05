2c37479b536631dc029be973681e8e25f30a570d
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-05 17:04:42 +0530

    the func copy and paste to new file as a backup

---
+++ information.py
@@ -0,0 +1,12 @@
+
+# Gentele introduction for a user
+
+def information():
+    note = print('Welcome to the Fintraa !')
+    name = input('Enter your name : ')
+    greets = input(f'''Hey {name} , Welcome to Fintraa !!  
+The Job Eater of Accountant and Personal Assitant !! ''') 
+    
+information()
+
+
