b7fcea7288daa366e33ecca27d78fb158a1a3704
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-05 16:43:26 +0530

    The V1,py contains the first version of this software

---
+++ V1.py
@@ -0,0 +1,23 @@
+
+import webbrowser
+
+def open_sheet():
+    sheet_url = 'https://sheets.google.com'
+    print(f'Openning sheet .....')
+    webbrowser.open(sheet_url)
+
+
+name = input('''Hey , What's your name? :''')
+print(f'Welcome {name} This side your personal finance bro: ')
+print('')
+
+choice1 = input('''What's your agenda today?.... press enter to continue ''')
+sheet_choice = input('''Press 'S' to open Google Sheets ''')
+
+# choice = input('''What's your agenda today? ''')
+
+if(sheet_choice ==  'S'):
+    open_sheet()
+
+else:
+    print('Something went wrong !')
