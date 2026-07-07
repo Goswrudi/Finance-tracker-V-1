bd785950898c30b0275e98673e43fb362b93619b
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-07 22:45:29 +0530

    added the all modifed files

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ class googlefuncs:
         docs_url = 'https://docs.google.com'
         webbrowser.open(docs_url)
 
+<<<<<<< HEAD
     def O_ppt():
         ppt_url = 'https://docs.google.com/presentation/u/0/'
         webbrowser.open(ppt_url)
@@ -62,6 +63,39 @@ class googlefuncs:
 #     pptx_url = 'https://powerpoint.cloud.microsoft/en-gb/'
 #     open = webbrowser.open(pptx_url)
 
+=======
+class googlefuncs:
+
+    def O_sheets():
+        sheets_url = 'https://sheets.google.com'
+        webbrowser.open(sheets_url)
+
+    def O_docs():
+        docs_url = 'https://docs.google.com'
+        webbrowser.open(docs_url)
+
+    def O_ppt():
+        ppt_url = 'https://docs.google.com/presentation/u/0/'
+        webbrowser.open(ppt_url)
+
+    def O_cal():
+        cal_url = 'https://calendar.google.com/'
+        webbrowser.open(cal_url)
+
+
+# def O_excel():
+#     excel_url = 'https://excel.cloud.microsoft/en-gb/'
+#     open = webbrowser.open(excel_url)
+
+# def O_word():
+#     word_url = 'https://word.cloud.microsoft/en-gb/'
+#     open = webbrowser.open(word_url)
+
+# def O_powerpoint():
+#     pptx_url = 'https://powerpoint.cloud.microsoft/en-gb/'
+#     open = webbrowser.open(pptx_url)
+
+>>>>>>> 8116867 (modifed the file)
 # def O_MO():
 #     MO_url = 'https://www.microsoft.com/en-in/microsoft-365'
 #     open = webbrowser.open(MO_url)
