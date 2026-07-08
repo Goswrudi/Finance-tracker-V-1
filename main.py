d011f2d7036c56740f2a506a0fbf1c3f9c63b3ca
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-08 18:40:05 +0530

    Added the @static method to all classes

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 # Information for the user
 
 import webbrowser
+from datetime import datetime
 
 def information():
     note = print('Welcome to the Fintraa !')
     name = input('Enter your name : ')
     greets = print(f'''Hey {name} , Welcome to Fintraa !!  
-The Job Eater of Accountant and Personal Assitant !! ''')     
+The Job Eater of Accountant and Personal Assitant !! ''')   
+    return name   
 information()
 
 def setting():
@@ -32,40 +34,54 @@ setting()
 
 class googlefuncs:
 
+    @staticmethod
     def O_sheets():
         sheets_url = 'https://sheets.google.com'
         webbrowser.open(sheets_url)
-
+    
+    @staticmethod
     def O_docs():
         docs_url = 'https://docs.google.com'
         webbrowser.open(docs_url)
 
+    @staticmethod
     def O_ppt():
         ppt_url = 'https://docs.google.com/presentation/u/0/'
         webbrowser.open(ppt_url)
 
+    @staticmethod
     def O_cal():
         cal_url = 'https://calendar.google.com/'
         webbrowser.open(cal_url)
 
-# googlefuncs.O_cal()
-
 class microsoftfuncs:
 
+    @staticmethod
     def O_excel():
         excel_url = 'https://excel.cloud.microsoft/en-gb/'
         open = webbrowser.open(excel_url)
 
+    @staticmethod
     def O_word():
         word_url = 'https://word.cloud.microsoft/en-gb/'
         open = webbrowser.open(word_url)
 
+    @staticmethod
     def O_powerpoint():
         pptx_url = 'https://powerpoint.cloud.microsoft/en-gb/'
         open = webbrowser.open(pptx_url)
 
-        
+    @staticmethod
     def O_MO():
         MO_url = 'https://www.microsoft.com/en-in/microsoft-365'
         open = webbrowser.open(MO_url)
  
+
+# user_choice = input(f'''Mention Today's Agenda :''').upper
+
+# if(user_choice == 'S'):
+#     googlefuncs.O_cal()
+
+# elif(user_choice == 'none'.lower):
+#     print(f'No problem.... fintraa is ready for anything you want!!')
+
