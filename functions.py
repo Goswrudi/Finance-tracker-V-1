345a901f6de6a0ad9a2f8a776c5261d08beaf2ef
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-08 18:10:35 +0530

    added the microsoft functionsto the new file

--- functions.py
+++ functions.py
@@ -1,3 +1,23 @@
 # The purpose of creating this file is to maks sure not everything dfirectly writtend in main file
 # 
 
+
+class microsoftfuncs:
+
+    def O_excel():
+        excel_url = 'https://excel.cloud.microsoft/en-gb/'
+        open = webbrowser.open(excel_url)
+
+    def O_word():
+        word_url = 'https://word.cloud.microsoft/en-gb/'
+        open = webbrowser.open(word_url)
+
+    def O_powerpoint():
+        pptx_url = 'https://powerpoint.cloud.microsoft/en-gb/'
+        open = webbrowser.open(pptx_url)
+
+        
+    def O_MO():
+        MO_url = 'https://www.microsoft.com/en-in/microsoft-365'
+        open = webbrowser.open(MO_url)
+ 
