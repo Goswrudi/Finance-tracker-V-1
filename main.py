2ff40b529a53e7f979b47595bc085a9b052ba976
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-08 18:09:31 +0530

    added the class to microsoft funcs

--- main.py
+++ main.py
@@ -50,32 +50,22 @@ class googlefuncs:
 
 # googlefuncs.O_cal()
 
-# def O_excel():
-#     excel_url = 'https://excel.cloud.microsoft/en-gb/'
-#     open = webbrowser.open(excel_url)
+class microsoftfuncs:
 
-# def O_word():
-#     word_url = 'https://word.cloud.microsoft/en-gb/'
-#     open = webbrowser.open(word_url)
+    def O_excel():
+        excel_url = 'https://excel.cloud.microsoft/en-gb/'
+        open = webbrowser.open(excel_url)
 
-# def O_powerpoint():
-#     pptx_url = 'https://powerpoint.cloud.microsoft/en-gb/'
-#     open = webbrowser.open(pptx_url)
+    def O_word():
+        word_url = 'https://word.cloud.microsoft/en-gb/'
+        open = webbrowser.open(word_url)
 
+    def O_powerpoint():
+        pptx_url = 'https://powerpoint.cloud.microsoft/en-gb/'
+        open = webbrowser.open(pptx_url)
 
-# def O_excel():
-#     excel_url = 'https://excel.cloud.microsoft/en-gb/'
-#     open = webbrowser.open(excel_url)
-
-# def O_word():
-#     word_url = 'https://word.cloud.microsoft/en-gb/'
-#     open = webbrowser.open(word_url)
-
-# def O_powerpoint():
-#     pptx_url = 'https://powerpoint.cloud.microsoft/en-gb/'
-#     open = webbrowser.open(pptx_url)
-
-# def O_MO():
-#     MO_url = 'https://www.microsoft.com/en-in/microsoft-365'
-#     open = webbrowser.open(MO_url)
+        
+    def O_MO():
+        MO_url = 'https://www.microsoft.com/en-in/microsoft-365'
+        open = webbrowser.open(MO_url)
  
