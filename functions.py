85e743fb4a1e538fa2be431bc6d184d6e36af21d
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-08 18:11:57 +0530

    added the google functions and imported webrowser

--- functions.py
+++ functions.py
@@ -1,6 +1,26 @@
 # The purpose of creating this file is to maks sure not everything dfirectly writtend in main file
 # 
 
+import webbrowser
+
+class googlefuncs:
+
+    def O_sheets():
+        sheets_url = 'https://sheets.google.com'
+        webbrowser.open(sheets_url)
+
+    def O_docs():
+        docs_url = 'https://docs.google.com'
+        webbrowser.open(docs_url)
+
+    def O_ppt():
+        ppt_url = 'https://docs.google.com/presentation/u/0/'
+        webbrowser.open(ppt_url)
+
+    def O_cal():
+        cal_url = 'https://calendar.google.com/'
+        webbrowser.open(cal_url)
+
 
 class microsoftfuncs:
 
