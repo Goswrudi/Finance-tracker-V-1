80290d669c7442a381a4a57177ff998c9c220603
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-09 12:57:35 +0530

    Improved the code compatiblity

--- main.py
+++ main.py
@@ -1,87 +1,86 @@
-
-# Information for the user
-
 import webbrowser
 from datetime import datetime
 
 def information():
-    note = print('Welcome to the Fintraa !')
+    print('Welcome to Fintraa !')
     name = input('Enter your name : ')
-    greets = print(f'''Hey {name} , Welcome to Fintraa !!  
-The Job Eater of Accountant and Personal Assitant !! ''')   
-    return name   
-information()
+    print(f'''\nHey {name}, Welcome to Fintraa !!  
+The Job Eater of Accountants and Personal Assistants !!\n''')    
+    return name # Returning name so the app can remember it
 
-def setting():
-    G = print('''Google Web-Softwares key are ;
-S for Google Sheets 
-D for Google Docs
-P for Google PPT
-C for Google Calendor              
-GW for Google workspace              ''')
+def show_settings():
+    print('''Google Web-Softwares keys are:
+S  -> Google Sheets 
+D  -> Google Docs
+P  -> Google PPT
+C  -> Google Calendar               
+GW -> Google Workspace''')
 
-    print('')    
+    print('\n' + '='*30 + '\n')    
 
-    M = print('''Micorosft Web-Softwares key are ;
-e for Micorosft Excel 
-w for Micorosft Word 
-p for Microsoft Powerpoint
-MO for Microsoft one               
-              ''')
-    
-setting()
+    print('''Microsoft Web-Softwares keys are:
+E  -> Microsoft Excel 
+W  -> Microsoft Word 
+P_MS -> Microsoft Powerpoint
+MO -> Microsoft One 365''')
+    print('='*30 + '\n')
 
 
 class googlefuncs:
-
+    # @staticmethod tells Python these functions don't need 'self'
     @staticmethod
     def O_sheets():
-        sheets_url = 'https://sheets.google.com'
-        webbrowser.open(sheets_url)
-    
+        webbrowser.open('https://sheets.google.com')
+
     @staticmethod
     def O_docs():
-        docs_url = 'https://docs.google.com'
-        webbrowser.open(docs_url)
+        webbrowser.open('https://docs.google.com')
 
     @staticmethod
     def O_ppt():
-        ppt_url = 'https://docs.google.com/presentation/u/0/'
-        webbrowser.open(ppt_url)
+        webbrowser.open('https://docs.google.com/presentation/u/0/')
 
     @staticmethod
     def O_cal():
-        cal_url = 'https://calendar.google.com/'
-        webbrowser.open(cal_url)
+        webbrowser.open('https://calendar.google.com/')
+
 
 class microsoftfuncs:
-
     @staticmethod
     def O_excel():
-        excel_url = 'https://excel.cloud.microsoft/en-gb/'
-        open = webbrowser.open(excel_url)
+        webbrowser.open('https://excel.cloud.microsoft/en-gb/')
 
     @staticmethod
     def O_word():
-        word_url = 'https://word.cloud.microsoft/en-gb/'
-        open = webbrowser.open(word_url)
+        webbrowser.open('https://word.cloud.microsoft/en-gb/')
 
     @staticmethod
     def O_powerpoint():
-        pptx_url = 'https://powerpoint.cloud.microsoft/en-gb/'
-        open = webbrowser.open(pptx_url)
+        webbrowser.open('https://powerpoint.cloud.microsoft/en-gb/')
 
     @staticmethod
     def O_MO():
-        MO_url = 'https://www.microsoft.com/en-in/microsoft-365'
-        open = webbrowser.open(MO_url)
- 
+        webbrowser.open('https://www.microsoft.com/en-in/microsoft-365')
 
-# user_choice = input(f'''Mention Today's Agenda :''').upper
 
-# if(user_choice == 'S'):
+# # --- Main logic ---
+# username = information()
+# show_settings()
+
+# # Capture input and immediately force it to uppercase using ()
+# user_choice = input("Mention Today's Agenda (Enter Key): ").upper()
+
+# # Handle choice logic
+# if user_choice == 'S':
+#     print("Opening Google Sheets...")
+#     googlefuncs.O_sheets() # Fixed to call Sheets!
+
+# elif user_choice == 'C':
+#     print("Opening Google Calendar...")
 #     googlefuncs.O_cal()
 
-# elif(user_choice == 'none'.lower):
-#     print(f'No problem.... fintraa is ready for anything you want!!')
+# elif user_choice == 'NONE':
+#     print(f'\nNo problem, {username}... Fintraa is ready for anything you want!!')
 
+# else:
+#     print("\nInvalid key! Fintraa automation terminated.")
