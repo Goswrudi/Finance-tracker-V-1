830de11e857c9d0f7101c13a247ca7fe41df9e32
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-10 19:33:42 +0530

    redefined what i want

--- statistics.py
+++ statistics.py
@@ -1,1 +1,17 @@
 # Create some functios methods and functions which will help fintraa for the statistics overview 
+
+import numpy as np
+
+
+numbers = input('Enter a number : ') 
+print(numbers)
+
+
+# convert the numbers from string to list
+numbers = input("Enter numbers separated by commas: ")
+
+numbers = numbers.split(",")
+
+numbers = [float(x) for x in numbers]
+
+print(numbers)
