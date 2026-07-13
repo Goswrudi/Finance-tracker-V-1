560ca0e7a10e879951cd3712e00f8aac49cf5d87
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-13 17:10:17 +0530

    added the setting func

--- code_class.py
+++ code_class.py
@@ -1,73 +1,18 @@
-import webbrowser
+# Setting function
 
-def information():
-    note = print('Welcome to the Fintraa !')
-    name = input('Enter your name : ')
-    greets = print(f'''Hey {name} , Welcome to Fintraa !!  
-The Job Eater of Accountant and Personal Assitant !! ''')   
-    return name   
-information()
+def show_settings():
+    print('''Google Web-Softwares keys are:
+S  -> Google Sheets 
+D  -> Google Docs
+P  -> Google PPT
+C  -> Google Calendar               
+GW -> Google Workspace''')
 
-def setting():
-    G = print('''Google Web-Softwares key are ;
-S for Google Sheets 
-D for Google Docs
-P for Google PPT
-C for Google Calendor              
-GW for Google workspace              ''')
+    print('\n' + '='*30 + '\n')    
 
-    print('')    
-
-    M = print('''Micorosft Web-Softwares key are ;
-e for Micorosft Excel 
-w for Micorosft Word 
-p for Microsoft Powerpoint
-MO for Microsoft one               
-              ''')
-    
-setting()
-
-
-class googlefuncs:
-
-    @staticmethod
-    def O_sheets():
-        sheets_url = 'https://sheets.google.com'
-        webbrowser.open(sheets_url)
-    
-    @staticmethod
-    def O_docs():
-        docs_url = 'https://docs.google.com'
-        webbrowser.open(docs_url)
-
-    @staticmethod
-    def O_ppt():
-        ppt_url = 'https://docs.google.com/presentation/u/0/'
-        webbrowser.open(ppt_url)
-
-    @staticmethod
-    def O_cal():
-        cal_url = 'https://calendar.google.com/'
-        webbrowser.open(cal_url)
-
-class microsoftfuncs:
-
-    @staticmethod
-    def O_excel():
-        excel_url = 'https://excel.cloud.microsoft/en-gb/'
-        open = webbrowser.open(excel_url)
-
-    @staticmethod
-    def O_word():
-        word_url = 'https://word.cloud.microsoft/en-gb/'
-        open = webbrowser.open(word_url)
-
-    @staticmethod
-    def O_powerpoint():
-        pptx_url = 'https://powerpoint.cloud.microsoft/en-gb/'
-        open = webbrowser.open(pptx_url)
-
-    @staticmethod
-    def O_MO():
-        MO_url = 'https://www.microsoft.com/en-in/microsoft-365'
-        open = webbrowser.open(MO_url)
+    print('''Microsoft Web-Softwares keys are:
+E  -> Microsoft Excel 
+W  -> Microsoft Word 
+P_MS -> Microsoft Powerpoint
+MO -> Microsoft One 365''')
+    print('='*30 + '\n')
