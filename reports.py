86439eb68cf316b06cc2f01e3ec51200c56bfd9b
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-19 14:28:26 +0530

    The daily rpeort function is done

--- reports.py
+++ reports.py
@@ -1,16 +1,13 @@
-# The purpose of this file is to write code for the user weekly reports on credit and debits 
+# The purpose of this file is to write code for the user daily_reports on credit and debits 
 # file under the project of fiinntra...
 
 # Writing daily repots first 
 
-from code_class import weekly_reports
-
-weekly_reports()
-
-
 def daily():
     # 1. Ask for the total number of items first
     items_spend = int(input('On how many things did you spend total? (e.g., 2): '))
+
+    finalamount = 0
 
     # 2. Loop through each item one by one
     for i in range(items_spend):
@@ -21,8 +18,17 @@ def daily():
         
         # Ask for the money for THIS specific item
         money_spent = input(f'Enter money spent for {where_spend}: ')
-        
+
+        totalmoney = int(money_spent)
+
+        finalamount += totalmoney
+
         print(f'Recorded: You spent {money_spent} on {where_spend}.')
 
-daily()
+        print("\n===============================")
+        print(f"Your daily total spent is: {finalamount}")
+        print("===============================")
+        # total = input('Enter your total amount of money eg:(100,200)')
 
+
+daily()
