edb2856d216faa910b851287c70c7e7f1e40760d
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-21 15:36:27 +0530

    Imported the Weekly funciton

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import webbrowser
 from datetime import datetime
 
-from code_class import information, show_settings, googlefuncs, microsoftfuncs , calc_setting , daily_reports
+from code_class import information, show_settings, googlefuncs, microsoftfuncs , calc_setting , daily_reports , weekly_reports
 from calc import FinancialData, StatisticsCalculator, Arthmetic
-from reports import daily
+from reports import daily , weekly
 
 def logic_choice(choice):
     # --- GOOGLE & MICROSOFT PLUGINS ---
